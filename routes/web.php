8dfb66a83d009958e86f305d3d2796f74fa30e17
taiga-murakami <syanataiga.475@gmail.com>
2020-07-08 07:42:07 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,10 @@ Route::get('/', function () {
 });
 
 //ユーザー登録
-Route::get('signup', 'Aush\RegisterController@showRegistrationForm')->name('signup.get');
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+//認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('login.get');
